94fe7087cb3446acee65b6cb88ba0abe5de4d4eb
Alex <alex@planomatic.com>
2022-11-02 23:28:44 -0600

    Adding Flow and simple login/logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import "./flow/config";
+import { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
 
+  const [user, setUser] = useState({loggedIn: null})
 
+  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+
+  const AuthedState = () => {
+    return (
+      <div>
+          <a href={ 'https://flowscan.org/account/' + user?.addr } target="_blank" className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">{user?.addr ?? "No Address"}</a>
+          <a onClick={fcl.unauthenticate} className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Log Out</a>
+      </div>
+    )
+  }
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <a onClick={fcl.signUp} className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Login</a>
+      </div>
+    )
+  }
 
   // Navbar example from Flowbite --> https://flowbite.com/docs/components/navbar/
   return (
@@ -15,8 +39,10 @@ function App() {
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NFT Rentals</span>
               </a>
               <div className="flex items-center">
-                  <a href="tel:5541251234" className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-                  <a href="#" className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+                {user.loggedIn
+                  ? <AuthedState />
+                  : <UnauthenticatedState />
+                }
               </div>
           </div>
       </nav>
